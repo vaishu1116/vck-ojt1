b9105b1be4bf67f3de7a5ef7f6ba17ef1dd990e2
Vaishnavi Gaikwad <vaishnavigaikwad1116@gmail.com>
2025-06-12 11:26:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -40,16 +40,19 @@ import ContactPage from "./pages/ContactPage";
 import CoursesPage from "./pages/CoursesPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
 import Home from "./pages/Home";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
 const App = () => {
   return (
       <Router>
         <Routes>
+          <Route path="/" element = {<Home />} />
           <Route path="/home" element = {<Home />} />
           <Route path="/aboutpage" element = {<AboutPage/>} />
           <Route path="/coursespage" element = {<CoursesPage/>} />
           <Route path="/contactpage" element = {<ContactPage/>} />
           <Route path="/admissionspage" element = {<AdmissionsPage/>} />
         </Routes>
+        <ChatbotComponent/>
       </Router>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import './Footer.css';
 const Footer = () => {
     return(
+        
         <footer>
             <p>&copy; 2025 Vivekanand College. All rights reserved.</p>
         </footer>
